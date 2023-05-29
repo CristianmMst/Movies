556058e8d0474dfa7f189fb95d40e76e6eb0463d
CristianmMst <cristianmmst@gmail.com>
2023-05-29 12:33:30 -0500

    rename

--- client/src/pages/index.ts
+++ client/src/pages/index.ts
@@ -3,6 +3,6 @@ export * from "./Login";
 export * from "./SignIn";
 export * from "./Movies";
 export * from "./Search";
+export * from "./Detail";
 export * from "./Profile";
 export * from "./NotFound";
-export * from "./MovieDetail";
